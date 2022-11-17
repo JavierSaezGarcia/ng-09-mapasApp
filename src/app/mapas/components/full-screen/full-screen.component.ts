0b658cd67e7a6d5bee742c23913e50165988264a
Javier Sáez García <javiersaez1205@gmail.com>
2022-11-17 21:31:05 +0100

    Carga mapbox correcto

--- src/app/mapas/components/full-screen/full-screen.component.ts
+++ src/app/mapas/components/full-screen/full-screen.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+
 
 
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
-  styles: [
+  styles: [`
+  #map {
+    width:100%;
+    height:100vh;
+    position:absolute;
+  }
+  `
   ]
 })
 export class FullScreenComponent implements OnInit {
@@ -12,6 +21,16 @@ export class FullScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-0.2291876,39.6702607],
+    zoom: 16
+       
+
+
+  });
   }
 
 }
